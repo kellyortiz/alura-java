4cd4261a10db11af0b653582293d370445f507d6
Kelly <keortizf@gmail.com>
2023-01-10 23:42:44 -0300

    Criação do workspace e aula 04 da alura
    Nesta aula foi aprendido como criar variaveis do tipo int e como utiliza-la

---
+++ AulasAlura/FormacaoAlura/src/aula04/topico1.java
@@ -0,0 +1,22 @@
+package aula04;
+
+public class topico1 {
+
+	public static void main(String[] args) {
+	
+		int idade;
+		
+		idade = 37;
+	
+		System.out.println(idade);
+	
+		idade = 30 + 10;
+	
+		System.out.println(idade);
+	
+		idade = (7 * 5) +  2;
+	
+		System.out.println(idade);
+
+	}
+}
---
+++ AulasAlura/FormacaoAlura/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kellyortiz
+ *
+ */
+module FormacaoAlura {
+}
